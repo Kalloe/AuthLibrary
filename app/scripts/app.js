c615328d331985e955a466842083606d44579a06
Haylee Heilman <hayleeheilman@Haylees-MacBook-Pro.local>
2017-09-23 14:51:03 -0600

    Started project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name authLibraryApp
+ * @description
+ * # authLibraryApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('authLibraryApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name authLibraryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the authLibraryApp
+ */
+angular.module('authLibraryApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
